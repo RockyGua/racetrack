62fcef2fb2ec943d0ea89d3d8d5006a0b50845cd
RockyGua <m_xiaokong@126.com>
2015-07-12 17:43:31 +0800

    change dataSource to mysql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,43 +1,35 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "grails"
+    password = "server"
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.use_query_cache = true
+    cache.provider_class ='com.opensymphony.oscache.hibernate.OSCacheProvider'
 }
 // environment specific settings
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            // one of 'create', 'create-drop','update'
+            dbCreate = "create-drop"
+// NOTE: the JDBC connection string should be
+// all on the same line.
+            url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
         }
     }
     production {
         dataSource {
-//            dbCreate = "update"
-//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//            pooled = true
-//            properties {
-//               maxActive = -1
-//               minEvictableIdleTimeMillis=1800000
-//               timeBetweenEvictionRunsMillis=1800000
-//               numTestsPerEvictionRun=3
-//               testOnBorrow=true
-//               testWhileIdle=true
-//               testOnReturn=true
-//               validationQuery="SELECT 1"
-//            }
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
         }
     }
 }
