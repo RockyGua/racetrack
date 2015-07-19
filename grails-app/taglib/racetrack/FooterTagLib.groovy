5dde5e0900c185ad0805446ebd2798654a101568
RockyGua <m_xiaokong@126.com>
2015-07-19 22:46:09 +0800

    Define tagLib

---
+++ grails-app/taglib/racetrack/FooterTagLib.groovy
@@ -0,0 +1,12 @@
+package racetrack
+
+class FooterTagLib {
+    def thisYear = {
+        out << new Date().format("yyyy")
+    }
+
+    def copyRight = {attrs, body ->
+        out << "&copy; " + attrs.startYear + " - "
+        out << thisYear() + " " + body()
+    }
+}
